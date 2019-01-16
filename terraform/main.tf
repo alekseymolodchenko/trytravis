9b221e7610b1645c6048655b004f41914fd7a9b1
Aleksey Molodchenko <alexey.sw1tch@gmail.com>
2019-01-16 10:15:31 +0300

    feat(terraform): add ssh key to project metadate

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,12 @@ provider "google" {
   region  = "${var.region}"
 }
 
+resource "google_compute_project_metadata" "ssh_keys" {
+  metadata {
+    ssh-keys = "appuser1:${file(var.public_key_path)}"
+  }
+}
+
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
