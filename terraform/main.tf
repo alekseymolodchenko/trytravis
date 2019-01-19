a52380c0b8584e753f89d82d4bb124115b2e3f2c
Aleksey Molodchenko <alexey.sw1tch@gmail.com>
2019-01-19 13:41:47 +0300

    feat(terraform): add resource from reddit-app2

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,6 +54,50 @@ resource "google_compute_instance" "app" {
   }
 }
 
+resource "google_compute_instance" "app2" {
+  name         = "reddit-app2"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+
+  tags = ["reddit-app"]
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
 
