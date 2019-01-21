8b491509180caf558e432608f9f1e06c4b546f59
Aleksey Molodchenko <alexey.sw1tch@gmail.com>
2019-01-21 23:06:53 +0300

    refactor(terraform): some project refactoring

---
+++ terraform/app.tf
@@ -0,0 +1,45 @@
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+}
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,10 +11,6 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable disk_image {
-  description = "Disk image"
-}
-
 variable "private_key_path" {
   description = "Path to the private key used for ssh access"
 }
@@ -27,4 +23,14 @@ variable "zone" {
 variable "app_count" {
   description = "Application resource count"
   default     = 1
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db"
 }
