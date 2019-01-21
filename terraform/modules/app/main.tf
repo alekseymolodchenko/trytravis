fa8852b7d835e51f34ec9ba4fbc18e5de0825e57
Aleksey Molodchenko <alexey.sw1tch@gmail.com>
2019-01-21 23:49:07 +0300

    feat(terraform): add vpc module

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -22,24 +22,7 @@ resource "google_compute_instance" "app" {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 }
+
 resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip"
-}
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
-
-  # Название сети, в которой действует правило
-  network = "default"
-
-  # Какой доступ разрешить
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  # Каким адресам разрешаем доступ
-  source_ranges = ["0.0.0.0/0"]
-
-  # Правило применимо для инстансов с перечисленными тэгами
-  target_tags = ["reddit-app"]
 }
--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,4 +1,3 @@
-
 resource "google_compute_instance" "db" {
   name         = "reddit-db"
   machine_type = "g1-small"
@@ -19,18 +18,4 @@ resource "google_compute_instance" "db" {
   metadata {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
-}
-
-# Правило firewall
-resource "google_compute_firewall" "firewall_mongo" {
-  name    = "allow-mongo-default"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["27017"]
-  }
-
-  target_tags = ["reddit-db"]
-  source_tags = ["reddit-app"]
 }
