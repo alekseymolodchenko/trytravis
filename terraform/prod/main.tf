4ba747bdefe494e8f4620c787952443b8d4d142a
Aleksey Molodchenko <alexey.sw1tch@gmail.com>
2019-01-23 23:22:31 +0300

    fix(terraform): update stage & prod env

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -7,18 +7,21 @@ provider "google" {
 module "app" {
   source          = "../modules/app"
   public_key_path = "${var.public_key_path}"
+	private_key_path = "${var.private_key_path}"
   zone            = "${var.zone}"
   app_disk_image  = "${var.app_disk_image}"
+	db_address  = "${module.db.db_internal_ip}"
 }
 
 module "db" {
   source          = "../modules/db"
   public_key_path = "${var.public_key_path}"
+	private_key_path = "${var.private_key_path}"
   zone            = "${var.zone}"
   db_disk_image   = "${var.db_disk_image}"
 }
 
 module "vpc" {
   source        = "../modules/vpc"
-  source_ranges = ["66.249.81.118"]
+  source_ranges = "${var.source_ranges}"
 }
--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -11,18 +11,13 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable "private_key_path" {
+variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
 
-variable "zone" {
+variable zone {
   description = "Zone"
   default     = "europe-west1-b"
-}
-
-variable "app_count" {
-  description = "Application resource count"
-  default     = 1
 }
 
 variable app_disk_image {
@@ -33,4 +28,24 @@ variable app_disk_image {
 variable db_disk_image {
   description = "Disk image for reddit db"
   default     = "reddit-db"
+}
+
+variable app_count {
+  description = "Application resource count"
+  default     = 1
+}
+
+variable db_count {
+  description = "Database resource count"
+  default     = 1
+}
+
+variable env {
+  description = "Environment variable"
+  default     = "prod"
+}
+
+variable "source_ranges" {
+	description = "Allowed IP addresses"
+  default     = ["93.159.232.194/32"]
 }
