3a589f7e912de21bf1503b45ca34b9a5d8d7e14c
Aleksey Molodchenko <alexey.sw1tch@gmail.com>
2019-01-23 23:20:16 +0300

    feat(terraform): add variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,16 +11,16 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable "private_key_path" {
+variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
 
-variable "zone" {
+variable zone {
   description = "Zone"
   default     = "europe-west1-b"
 }
 
-variable "app_count" {
+variable app_count {
   description = "Application resource count"
   default     = 1
 }
@@ -33,4 +33,9 @@ variable app_disk_image {
 variable db_disk_image {
   description = "Disk image for reddit db"
   default     = "reddit-db"
+}
+
+variable "source_ranges" {
+	description = "Allowed IP addresses"
+  default     = ["0.0.0.0/0"]
 }
