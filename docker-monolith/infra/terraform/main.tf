4c5cb134265f1c15392c7bd8e3cab8d5cbc79994
Aleksey Molodchenko <alexey.sw1tch@gmail.com>
2019-02-13 17:49:33 +0300

    feat(terraform): update instance zone

---
+++ docker-monolith/infra/terraform/main.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "app" {
+  count        = "${var.app_count}"
+  name         = "reddit-app-docker-${format("%02d", count.index+1)}"
+  machine_type = "${var.app_machine_type}"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app-docker"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
---
+++ docker-monolith/infra/terraform/vpc.tf
@@ -0,0 +1,14 @@
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+
+  target_tags = ["reddit-app-docker"]
+}
